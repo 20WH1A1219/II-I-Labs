8927774cf820d4ab3a625b4f35489b0b05758612
20WH1A1219 <96655092+20WH1A1219@users.noreply.github.com>
2022-01-28 16:06:12 +0530

    Create LateBinding.cpp

---
+++ C++ Lab/Experiment 12(b)/LateBinding.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class Base{
+      public:
+          int a;
+          void read(){
+               cout<<"Enter the value of 'a':"<<endl;
+               cin>>a;
+          }
+          void display(){
+               cout<<"Value of 'a':"<<a<<endl;
+          }
+};
+class child : public Base{
+       public:
+           int b;
+           child(){
+               a = 45;
+           }
+           void read(){
+                cout<<"Enter the value of'b':"<<endl;
+                cin >> b;
+           }
+           void display(){
+                cout<<"Value of 'a':"<<a<<endl<<"Value of 'b':"<<b<<endl;
+           }
+};
+int main(){
+    Base obj;
+    child obj1;
+    obj.read();
+    obj.display();
+    obj1.read();
+    obj1.display();
+    return 0;
+}
